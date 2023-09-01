3a48ae687531a8df5605d106fa4056290852b4ae
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-09-01 15:37:33 +0900

    NHN_Mart 실습 테스트 코드 추가 커밋(2)

---
+++ NHN_Mart/src/main/java/org/nhnMart/Customer.java
@@ -0,0 +1,22 @@
+package org.nhnMart;
+
+public class Customer {
+    private int money;
+
+    Customer(int m) {
+        this.money = m;
+    }
+
+    public void isBuy(int m){
+        int newMoney = this.money - m;
+        if (newMoney < 0) {
+           throw new IllegalArgumentException("돈이 부족합니다.");
+        } else {
+            this.money = newMoney;
+        }
+    }
+
+    public int getMoney(){
+        return this.money;
+    }
+}
