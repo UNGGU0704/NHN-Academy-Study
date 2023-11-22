9daa71af6ed889e6e1304f99002462cf012af6fd
UNGGU0704 <130115689+UNGGU0704@users.noreply.github.com>
2023-11-22 14:14:47 +0900

    refactor : servlet 실습 파일 변경

---
+++ servlet-jsp/servlet/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+I
+src/main/webapp/index.jsp,8/e/8e6d3b70c9f5b02570779b0441540d45eceb0ded
+O
+src/main/webapp/WEB-INF/web.xml,2/e/2e22b6edd746fa3dc40ad7276c87c8d87d72a392
+d
+4src/main/java/com/nhnacademy/study/HelloServlet.java,4/4/44f4d5363b94a6749271c59f193f13b11b201b8d
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
--- servlet-jsp/servlet/src/main/java/com/nhnacademy/study/HelloServlet.java
+++ servlet-jsp/servlet/src/main/java/com/nhnacademy/study/HelloServlet.java
@@ -1,21 +1,20 @@
-package com.nhnacademy.servleet;
+package com.nhnacademy.study;
 
 import java.io.*;
 
+import java.util.logging.Logger;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
+    private static Logger log = Logger.getLogger(HelloServlet.class.getName());
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("init 실행");
         response.setCharacterEncoding("utf-8");
         try(PrintWriter writer = response.getWriter()) {
             writer.println("<!DOCTYPE html>");
@@ -33,6 +32,21 @@ public class HelloServlet extends HttpServlet {
         }
     }
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        log.info("before init!");
+        super.init(config);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("before service!");
+        super.service(req, resp);
+    }
+
+    @Override
     public void destroy() {
+        log.info("before destroy!");
+        super.destroy();
     }
 }
